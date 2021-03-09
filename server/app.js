110f57e8b728db6e01f44fb11992d956f74006cc
chinonso <williamschinonso11@gmail.com>
2021-03-09 11:43:14 +0100

    install compression and allow express use compression middleware

--- server/app.js
+++ server/app.js
@@ -1,15 +1,21 @@
 'use strict';
-import express from "express";
-import helmet from "helmet";
-import bodyParser from "body-parser";
+import express from 'express';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import compression from 'compression'
 
 // Instantiate express server
 const app = express();
 
-app.disable("etag");
-app.disable("x-powered-by");
+app.disable('etag');
+app.disable('x-powered-by');
 app.use(helmet());
+
+// Parse URL-encoded bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Compress all routes
+app.use(compression());
 
 export default app;
