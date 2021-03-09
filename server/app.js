1c2f190a62abe3a14d3b29f029715b495bc304dd
chinonso <williamschinonso11@gmail.com>
2021-03-09 14:42:01 +0100

    add trimmer middleware

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import compression from 'compression'
+import trimmer from './middlewares/trimmer.js'
 
 // Instantiate express server
 const app = express();
@@ -17,5 +18,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Compress all routes
 app.use(compression());
+
+// removes whitespace from payload
+app.use(trimmer);
 
 export default app;
