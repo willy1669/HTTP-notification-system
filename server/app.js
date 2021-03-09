60c0e756b11a06de2685353b559f57316b100f51
chinonso <williamschinonso11@gmail.com>
2021-03-09 09:23:37 +0100

    install bodyParser and allow express use bodyParser middleware

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 'use strict';
 import express from "express";
 import helmet from "helmet";
+import bodyParser from "body-parser";
 
 // Instantiate express server
 const app = express();
@@ -8,5 +9,7 @@ const app = express();
 app.disable("etag");
 app.disable("x-powered-by");
 app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 export default app;
