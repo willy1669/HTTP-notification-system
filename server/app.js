8480eff69d70a7cd2c87b3f79511d1132188cc26
chinonso <williamschinonso11@gmail.com>
2021-03-09 09:21:30 +0100

    install helmet and allow express use helmet middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 'use strict';
 import express from "express";
+import helmet from "helmet";
 
 // Instantiate express server
 const app = express();
 
 app.disable("etag");
 app.disable("x-powered-by");
+app.use(helmet());
 
 export default app;
