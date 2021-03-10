322e8317afe02bb132c45cf14c696ff54913eef8
chinonso <williamschinonso11@gmail.com>
2021-03-10 08:52:13 +0100

    create subscribe function

---
+++ server/database/subscriber.js
@@ -0,0 +1,16 @@
+'use strict';
+import subscriberModel from '../models/subscriber';
+import topicModel from '../models/topic';
+
+export const subscriber = async (url, topic) => {
+    const result = await topicModel.findOne({topic});
+
+    if (result !== null) {
+        const response = await subscriberModel.create({url});
+        if (response) {
+            return {success: true, data: {url, topic}}
+        }
+        return {success: false, message: `Error.`}
+    }
+    return {success: false, message: `No topic found.`}
+}
